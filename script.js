0510d30bc44bdc46f6a1f021a05de85857d557a7
don0043 <daniiar.arkabaev@iaau.edu.kg>
2022-10-12 01:45:26 +0600

    audio indicator added

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
+const visualizer = document.querySelector('.visualizer')
+const sndVisualizer = document.querySelector('.mid-visualizer')
+const trdVisualizer = document.querySelector('.bot-visualizer')
 window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+
     if (!audio){
         return
     }
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
+    visualizer.classList.add('play');
+    sndVisualizer.classList.add('sndPlay')
+    trdVisualizer.classList.add('trdPlay')
 
 })
 
+
+
 function removeTransition(e){
     if (e.propertyName !== 'transform') return;
-    console.log(e.propertyName)
     this.classList.remove('playing')
+    visualizer.classList.remove('play')
+    sndVisualizer.classList.remove('sndPlay')
+    trdVisualizer.classList.remove('trdPlay')
+
 }
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+
+
+
